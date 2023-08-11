0c4e7c4f43dac5e6de9bb2e0d94b279808013147
JMarcos VInicius <viniciusdejesus565@gmail.com>
2023-08-11 14:36:21 -0300

    :lipstick: feat cria o TextFieldWidget

---
+++ lib/app/modules/login/presenter/login_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: Column(
+        children: [
+          Container(),
+        ],
+      ),
+    );
+  }
+}
