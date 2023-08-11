ccb6989d2a3cb3366ceb7a5b462b8d9b9f04d52f
JMarcos VInicius <viniciusdejesus565@gmail.com>
2023-08-11 16:29:15 -0300

    :wrench: chore configura o firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:anotei/app/modules/register/presenter/register_controller.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'app/modules/login/presenter/login_controller.dart';
 import 'app/modules/register/presenter/register_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp((MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (_) => LoginController()),
