0f4c249a95b1891645fb088b0c7fce574de26b17
JMarcos VInicius <viniciusdejesus565@gmail.com>
2023-08-15 10:37:48 -0300

    :lipstick: feat altera o layout da HomePage

--- lib/app/modules/home/presenter/home_page.dart
+++ lib/app/modules/home/presenter/home_page.dart
@@ -7,9 +7,73 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        backgroundColor: Color(0xFFD9ACF5),
+        title: Text(
+          'Anotei',
+          style: TextStyle(
+              color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+        ),
       ),
-      body: Container(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Material(
+        borderRadius: BorderRadius.circular(8.0),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(8.0),
+          onTap: () async {},
+          child: Ink(
+            width: 56.0,
+            height: 56.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(8.0),
+              color: Color(0xFFD9ACF5),
+            ),
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      backgroundColor: Color(0xFFFFCEFE),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: ListView.builder(
+          itemCount: 1,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+              key: UniqueKey(),
+              onDismissed: (direction) {},
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Color(0xFFD9ACF5),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Checkbox(
+                        value: false,
+                        onChanged: (value) {},
+                      ),
+                      Text(
+                        "data",
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
